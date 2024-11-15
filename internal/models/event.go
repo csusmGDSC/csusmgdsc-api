2bcbd6ca87e3da5b8c503b0017fd37688167d917
gabrieltellez <tellez.gabriel1709@gmail.com>
2024-11-14 21:42:32 -0800

    Feature: Event creation route

--- internal/models/event.go
+++ internal/models/event.go
@@ -8,26 +8,26 @@ import (
 
 type Event struct {
 	ID               uuid.UUID   `json:"id,omitempty"`
-	Title            string      `json:"title"`
+	Title            string      `json:"title" validate:"required"`
 	Room             *CSUSMRoom  `json:"room,omitempty"`
 	Tags             []string    `json:"tags,omitempty"`
-	StartTime        time.Time   `json:"startTime"`
-	EndTime          time.Time   `json:"endTime"`
-	Type             int         `json:"type"`
+	StartTime        time.Time   `json:"startTime" validate:"required"`
+	EndTime          time.Time   `json:"endTime" validate:"required"`
+	Type             int         `json:"type" validate:"required"`
 	Location         *string     `json:"location,omitempty"`
-	Date             time.Time   `json:"date"`
+	Date             time.Time   `json:"date" validate:"required"`
 	GithubRepo       *string     `json:"githubRepo,omitempty"`
 	SlidesURL        *string     `json:"slidesURL,omitempty"`
 	ImageSrc         *[]byte     `json:"imageSrc,omitempty"`
 	VirtualURL       *string     `json:"virtualURL,omitempty"`
 	ExtraImageSrcs   [][]byte    `json:"extraImageSrcs,omitempty"`
-	Description      string      `json:"description"`
+	Description      string      `json:"description" validate:"required"`
 	About            *string     `json:"about,omitempty"`
 	AttendeeIds      []uuid.UUID `json:"attendeeIds,omitempty"`
-	OrganizerIds     []uuid.UUID `json:"organizerIds"`
+	OrganizerIds     []uuid.UUID `json:"organizerIds" validate:"required"`
 	UsersAttendedIds []uuid.UUID `json:"usersAttendedIds,omitempty"`
-	CreatedAt        time.Time   `json:"createdAt"`
-	UpdatedAt        time.Time   `json:"updatedAt"`
+	CreatedAt        time.Time   `json:"createdAt,omitempty"`
+	UpdatedAt        time.Time   `json:"updatedAt,omitempty"`
 	CreatedBy        *uuid.UUID  `json:"createdBy,omitempty"`
 }
 

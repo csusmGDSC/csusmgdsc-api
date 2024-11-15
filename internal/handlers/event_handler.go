f4a048fc33a5ac14cd800059051cd583334a666e
gabrieltellez <tellez.gabriel1709@gmail.com>
2024-11-14 21:42:32 -0800

    Feature: Event creation route

--- internal/handlers/event_handler.go
+++ internal/handlers/event_handler.go
@@ -3,23 +3,44 @@ package handlers
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/csusmGDSC/csusmgdsc-api/internal/db"
 	"github.com/csusmGDSC/csusmgdsc-api/internal/db/repositories"
 	"github.com/csusmGDSC/csusmgdsc-api/internal/models"
+	"github.com/go-playground/validator"
 	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 )
 
+var validate *validator.Validate
+
+func init() {
+	validate = validator.New()
+}
+
 func InsertEventHandler(c echo.Context) error {
 	var event models.Event
-
 	// Bind JSON request body to the Event struct
 	if err := c.Bind(&event); err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request body"})
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	// Validate the struct
+	if err := validate.Struct(event); err != nil {
+		var validationErrors []string
+		for _, err := range err.(validator.ValidationErrors) {
+			validationErrors = append(validationErrors, err.Field()+" "+err.Tag())
+		}
+
+		return c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"errors": validationErrors,
+		})
 	}
 
 	event.ID = uuid.New()
+	event.CreatedAt = time.Now()
+	event.UpdatedAt = time.Now()
 
 	dbConnection, err := db.ConnectDB()
 	if err != nil {

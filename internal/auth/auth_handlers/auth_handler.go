a1f50c5c107ce94b149f9e84196e4bb288677f20
TarigElamin2000 <tariq.baha558@gmail.com>
2025-02-17 22:33:27 -0800

    removing debugging statment

--- internal/auth/auth_handlers/auth_handler.go
+++ internal/auth/auth_handlers/auth_handler.go
@@ -52,7 +52,7 @@ func (h *OAuthHandler) RegisterUser(c echo.Context) error {
 
 	err = auth_utils.SendVerificationEmail(user.Email, verificationToken)
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to send verification email:" + err.Error()})
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to send verification email"})
 	}
 
 	return c.JSON(http.StatusCreated, user)
